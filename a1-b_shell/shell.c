1527e6336db0790e761c12edddf3b6eb7c3a6e7a
Thomas Willingham <twillingham@crimson.ua.edu>
2017-02-04 00:29:17 -0600

    shell

--- a1-b_shell/shell.c
+++ a1-b_shell/shell.c
@@ -3,8 +3,18 @@
 #include <unistd.h>
 #define MAX_LINE 80 // The maximum length command 
 #define HISTORY_SIZE 10 
+
+
+void parseCommand(char commandBuffer[], char *args[], int *background)
+{
+    int comLength;
+    
+    comLength = read(STDIN_FILENO, commandBuffer, MAX_LINE);
+}
+
 int main(void)
 {
+    char commandBuffer[MAX_LINE];
 	char *args[MAX_LINE/2 + 1]; // command line arguments
 	int should_run = 1; // flag to determine when to exit program
     int background = 0; // flag to run in background
