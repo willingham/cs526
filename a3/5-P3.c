3a859fd4185897db1f958564633a2df150218df8
Thomas Willingham <thomas@tshows.us>
2017-02-26 15:19:53 -0600

    asdf

--- a3/5-P3.c
+++ a3/5-P3.c
@@ -17,10 +17,12 @@ typedef int buffer_item;
 buffer_item buffer[BUFFER_SIZE];
 pthread_mutex_t mutex;
 sem_t full, empty;
-int counter, in, out;
+int counter;
 
 void *producer(void *param);
+int insertItem(buffer_item item);
 void *consumer(void *param);
+int removeItem(buffer_item *item);
 
 int main(int argc, char **argv)
 {
@@ -36,7 +38,8 @@ int main(int argc, char **argv)
     pthread_mutex_init(&mutex, NULL);
     sem_init(&full, 0, 0);
     sem_init(&empty, 0, BUFFER_SIZE);
-    counter = in = out = 0;
+    counter = 0;
+    //printf("numProducer: %d, numConsumer: %d\n", numProducer, numConsumer);
 
     pthread_t producers[numProducer];
     pthread_t consumers[numConsumer];
@@ -63,10 +66,10 @@ int insertItem(buffer_item item)
     }
 }
 
-int remove_item(buffer_item *item)
+int removeItem(buffer_item *item)
 {
     if (counter > 0) {
-        *item = buffer[(counter - 1)];
+        *item = buffer[counter - 1];
         counter--;
         return 0;
     } else {
@@ -80,11 +83,11 @@ void *producer(void *param)
     while (1) {
         sem_wait(&empty);
         pthread_mutex_lock(&mutex);
-        sleep(rand() % 8 + 1);
+        sleep(rand() % 5 + 1);
         item = rand();
 
         if (insertItem(item)) {
-            fprintf(stderr, " Producer error\n");
+            fprintf(stderr, "Producer error\n");
         } else {
             printf("Producer produced %d\n", item);
         }
@@ -99,9 +102,9 @@ void *consumer(void *param) {
     while (1) {
         sem_wait(&full);
         pthread_mutex_lock(&mutex);
-        sleep(rand() % 8 + 1);
+        sleep(rand() % 5 + 1);
 
-        if (remove_item(&item)) {
+        if (removeItem(&item)) {
             fprintf(stderr, "Consumer error\n");
         } else {
             printf("Consumer consumed %d\n", item);
